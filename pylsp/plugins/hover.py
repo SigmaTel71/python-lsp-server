17d371ef588931f1718b7ce8c6e26e78d63a1d41
Vitaly Orekhov <vkvo2000@vivaldi.net>
2026-04-29 23:31:53 +0300

    Variables of builtin types showing their value instead of docstring
    Initial implementation of better variable data lookup.
    docstring will continue to appear only if cursor was set on bare value.

--- pylsp/plugins/hover.py
+++ pylsp/plugins/hover.py
@@ -11,6 +11,7 @@ log = logging.getLogger(__name__)
 @hookimpl
 def pylsp_hover(config, document, position):
     signature_config = config.settings().get("signature", {})
+    settings = config.plugin_settings("jedi_definition")
     code_position = _utils.position_to_jedi_linecolumn(document, position)
     definitions = document.jedi_script(use_document_path=True).infer(**code_position)
     word = document.word_at_position(position)
@@ -40,6 +41,25 @@ def pylsp_hover(config, document, position):
         ),
         "",
     )
+    
+    # If the variable type is in builtin module, it will be reasonable
+    # to show its value instead of docstring.
+    if definition.in_builtin_module():
+        names = document.jedi_script(use_document_path=True).goto(
+            follow_imports=settings.get("follow_imports", True),
+            follow_builtin_imports=settings.get("follow_builtin_imports", True),
+            **code_position)
+        
+        if names:
+            name = names[0]
+            return {
+                "contents": _utils.format_docstring(
+                    f"{definition.name} {name.description}",
+                    preferred_markup_kind,
+                    signatures=[signature] if signature else None,
+                    signature_config=signature_config,
+                )
+            }
 
     include_docstring = signature_config.get("include_docstring", True)
 
